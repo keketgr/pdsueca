7c394bc436eebdf61dde3669391f312f8cad7d94
rafaelperdigaoneves@gmail.com <rafaelperdigaoneves@gmail.com@a0aa1fc1-209c-fb42-fbd6-3144d96c2981>
2013-01-04 21:03:50 +0000

    Cliente

--- Sistema/src/sistema/Observer.java
+++ Sistema/src/sistema/Observer.java
@@ -1,17 +1,17 @@
+package sistema;
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package sistema;
 
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import Jinterface.ServerInterface;
 
 /**
  *
@@ -22,46 +22,50 @@ public class Observer extends UnicastRemoteObject implements ObserverInterface {
     static Observer rmi;
 
     public Observer() throws RemoteException {
-    
     }
-
     /**
      * @param args the command line arguments
      */
     static ServerInterface servico;
+
     public static void main(String[] args) {
-        
+
         if (args.length > 0) {
-            String url = "rmi://" + args[0] + "/Serverservice";
+            String url ="rmi://localhost/ServerInterface";
+            System.out.println(args[0]);
             try {
-                servico= (ServerInterface) Naming.lookup(url);
-                
+                servico = (ServerInterface) Naming.lookup(url);
+
             } catch (Exception e) {
                 System.out.println("erro: " + e);
             }
-            
+
         } else {
             System.exit(-1);
         }
-        
-        Scanner sc=new Scanner(System.in);
-        String in="";
-        while(in!="sair"){
-            in=sc.next();
-            if(in.equals("jogadores")){
+        try {
+            servico.addObserver(new Observer());
+        } catch (RemoteException ex) {
+            Logger.getLogger(Observer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Scanner sc = new Scanner(System.in);
+        String in = "";
+        while (in != "sair") {
+            in = sc.next();
+            if (in.equals("jogadores")) {
                 try {
                     getjogadores();
                 } catch (RemoteException ex) {
                     Logger.getLogger(Observer.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            }else if(in.equals("jogadores")){
+            } else if (in.equals("jogos")) {
                 try {
                     getjogos();
                 } catch (RemoteException ex) {
                     Logger.getLogger(Observer.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-                
+
         }
     }
 
@@ -72,22 +76,19 @@ public class Observer extends UnicastRemoteObject implements ObserverInterface {
 
     @Override
     public void printjogo(String jgs) throws RemoteException {
-        System.out.println("Novo jogo " + jgs + " jogadores: ");
-        System.out.println(jgs);
-        
+        System.out.println(jgs );
     }
 
-    
     public static void getjogadores() throws RemoteException {
-        ArrayList<String> a=servico.getjogadores();
-        for(int i=0;i<a.size();i++){
+        ArrayList<String> a = servico.getjogadores();
+        for (int i = 0; i < a.size(); i++) {
             System.out.println(a.get(i));
         }
     }
 
     public static void getjogos() throws RemoteException {
-         ArrayList<String> a=servico.getjogos();
-        for(int i=0;i<a.size();i++){
+        ArrayList<String> a = servico.getjogos();
+        for (int i = 0; i < a.size(); i++) {
             System.out.println(a.get(i));
         }
     }
--- Sistema/src/sistema/ObserverInterface.java
+++ Sistema/src/sistema/ObserverInterface.java
@@ -1,10 +1,9 @@
+package sistema;
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package sistema;
 import java.rmi.*;
-import java.util.ArrayList;
 /**
  *
  * @author Paulo
